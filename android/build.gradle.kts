911e8ebc49ed2def4f8c3313c558ac902ae9def3
felle-dev <realfelle@proton.me>
2025-12-22 19:44:41 +0700

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,6 +20,12 @@ allprojects {
             force("com.android.tools.build:gradle:8.7.3")
             force("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
             force("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
+
+	    force("androidx.appcompat:appcompat:1.6.1")
+            force("androidx.appcompat:appcompat-resources:1.6.1")
+            force("androidx.drawerlayout:drawerlayout:1.2.0")
+            force("androidx.core:core:1.12.0")
+            force("androidx.fragment:fragment:1.6.2")
         }
     }
 }
